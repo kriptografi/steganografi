83c759d675eded7f6265e9b42b29b942aefea67c
Fadhil Imam Kurnia <fadhilimamk@gmail.com>
2018-02-11 20:38:08 +0700

    add some graphic

--- app.go
+++ app.go
@@ -5,5 +5,12 @@ import (
 )
 
 func main() {
+	fmt.Println("    _____ __")
+	fmt.Println("   / ___// /____  ____ _____ _____  ____ ")
+	fmt.Println("   \\__ \\/ __/ _ \\/ __ `/ __ `/ __ \\/ __ \\")
+	fmt.Println("  ___/ / /_/  __/ /_/ / /_/ / / / / /_/ /")
+	fmt.Println(" /____/\\__/\\___/\\__, /\\__,_/_/ /_/\\____/ ")
+	fmt.Println("               /____/                    ")
+	fmt.Println("Stegano version 1.0 2018-02-11\n")
 	fmt.Println("Hello!")
 }
